bb0ce78dd6dd1968e4ad82bf25996eb76348e9f8
Joy Cheng <xuejiao.j.cheng@gmail.com>
2018-06-03 16:15:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ var updateObjectWithKeyAndValue = function(object,key,value){
   
 }
 
+var destructivelyUpdateObjectWithKeyAndValue = function(object, key, value){
+  
+  object[key] = value;
+  return object
+}
